959abc78e66ace9c673d94f5e1b1e3aa0093c6bb
z789 <cfcjl@163.com>
2022-10-18 16:01:18 +0800

    fix check_file.h

--- check_file.sh
+++ check_file.sh
@@ -2,6 +2,6 @@
 KERNEL_VERSION=`uname  -r`
 
 if [[ ! -e include/mount.h ]]; then
-	echo "Please copy Kernel v${KERNEL_VERSION} to include/mount.h"
+	echo "Please copy Kernel v${KERNEL_VERSION} the file 'fs/mount.h' to include/mount.h"
 	exit -1
 fi
